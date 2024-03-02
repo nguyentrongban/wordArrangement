eb5a863ea1ff652be7fb1dc2a682791f24ec05ea
nguyentrongban <nguyentrongban01052003@gmail.com>
2024-03-02 18:27:36 +0700

    first commit

---
+++ Library.js
@@ -0,0 +1,14 @@
+// Import các module từ Firebase
+import { initializeApp } from 'https://www.gstatic.com/firebasejs/10.8.0/firebase-app.js';
+import { getFirestore, doc, getDoc } from 'https://www.gstatic.com/firebasejs/10.8.0/firebase-firestore.js';
+
+// Thay bằng cấu hình Firebase của bạn
+const firebaseConfig = {
+    apiKey: "AIzaSyD5LAva700pYcko_bDJLXAeAP5JFXszbAI",
+    authDomain: "xepchugame.firebaseapp.com",
+    projectId: "xepchugame",
+    storageBucket: "xepchugame.appspot.com",
+    messagingSenderId: "615621716578",
+    appId: "1:615621716578:web:27d08d769b75be95ecff3b",
+    measurementId: "G-87FMHJ27DG"
+};
